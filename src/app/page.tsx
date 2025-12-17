bd089e52e36cec681b72b62c5abd33199f83952a
avkumar-cloud <avkumar@bestpeers.com>
2025-12-17 16:32:01 +0530

    restructed src folder such that app and othere components are inside it

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Next.js + TypeScript + Tailwind 🚀
+      </h1>
+    </main>
+  );
+}
